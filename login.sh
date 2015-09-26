8c17719d7852f0a80440e2c526b62c04e5d42bb4
Oge Nnadi <oge@lbl.gov>
2015-09-26 00:12:26 -0700

    Make login.sh output unquoted session Id

--- login.sh
+++ login.sh
@@ -3,4 +3,5 @@
 curl -H "Content-Type: application/json" \
   -X POST \
   -d  '{email: "oge", password: "scot!syeP6"}' \
-  https://registry.jbei.org/rest/accesstoken | jq '.sessionId'
+  https://registry.jbei.org/rest/accesstoken | \
+  jq -r '.sessionId'
