3218195428b8e0ee841cd0042c1965dfeaf6e274
Oge Nnadi <oge@lbl.gov>
2015-09-30 02:26:18 -0700

    Zip all intermediate files and clean up working dir

--- make-ss.rb
+++ make-ss.rb
@@ -20,12 +20,25 @@ def score(index, clone, pool)
   index[clone][pool]['display']
 end
 
+def zip(dir, clone, pool)
+  intermediates = "analysis.bam analysis.bam.bai \
+      analysis.vcf analysis.bed analysis.fasta call score"
+
+  "cd #{dir} && \
+    zip #{clone}.ss.zip #{intermediates} && \
+    rm #{intermediates} && \
+    cd .."
+end
+
 index = JSON.parse(File.read(ARGV[0]))
 
 index.keys.each do |clone|
   index[clone].keys.each do |pool|
-    puts "#{svelt_cmd(pool, clone, pool)} && \
+    cmd = "#{svelt_cmd(pool, clone, pool)} && \
       #{call_cmd(pool, call(index, clone, pool))} && \
-      #{score_cmd(pool, score(index, clone, pool))}"
+      #{score_cmd(pool, score(index, clone, pool))} && \
+      #{zip(pool, clone, pool)}"
+    puts cmd
+    `#{cmd}`
   end
 end
