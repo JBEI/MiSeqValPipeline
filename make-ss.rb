310422f98697a9fde0bee2bbe7e52a38af538992
Oge Nnadi <oge@lbl.gov>
2015-09-30 01:43:20 -0700

    Add real score and call to ss.zip

--- make-ss.rb
+++ make-ss.rb
@@ -1,19 +1,30 @@
+require 'json'
+
 def svelt_cmd(dir, clone, pool)
   "python ../MiSeqValPipeline/svelt.py MS #{dir} #{clone} #{pool} #{dir}/analysis"
 end
 
-def call(dir, call)
+def call_cmd(dir, call)
   "echo #{call} > #{dir}/call"
 end
 
-def score(dir, score)
+def score_cmd(dir, score)
   "echo #{score} > #{dir}/score"
+end
+
+def call(index, clone, pool)
+  index[clone][pool]['call']
+end
+
+def score(index, clone, pool)
+  index[clone][pool]['display']
 end
 
 dir = ARGV[0]
 clone = ARGV[1]
 pool = ARGV[2]
+index = JSON.parse(File.read(ARGV[3]))
 
-puts "#{svelt_cmd(ARGV[0], ARGV[1], ARGV[2])} && \
-  #{call(dir, 'bae')} && \
-  #{score(dir, 419)}"
+puts "#{svelt_cmd(dir, clone, pool)} && \
+  #{call_cmd(dir, call(index, clone, pool))} && \
+  #{score_cmd(dir, score(index, clone, pool))}"
